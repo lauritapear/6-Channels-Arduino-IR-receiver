fc481896a91d2216315768a7a5767b8f1940d858
LauraArtunduaga <Laura.Artunduaga@ge.com>
2016-02-28 20:30:27 -0500

    IR receiver working :)

---
+++ IRServoController.ino
@@ -0,0 +1,44 @@
+#include <Servo.h>
+#include <IRremote.h>
+#include <IRremoteInt.h>
+#include <avr/wdt.h>
+#include "IRServoController.h"
+#include "ServoMonitor.h"
+#include "IRManager.h"
+
+int RECV_PIN = 11;
+int ledPin = 13;
+int relayOutputPin = 8;
+
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void InitializePins() {
+  
+    pinMode(relayOutputPin, OUTPUT);
+    pinMode(ledPin, OUTPUT);
+}
+
+void setup() {
+   
+    AttachServos();
+    InitializePins();
+    
+    Serial.begin(9600);
+    irrecv.enableIRIn();
+    
+    SetInitialValuesForServoMotors();
+    wdt_enable(WDTO_1S); //Do we really need this?
+}
+
+void loop() {
+    
+    if (irrecv.decode(&results)) {
+
+       TranslateIR(&results);
+        Serial.println(results.value, HEX);
+        irrecv.resume(); // Receive the next value
+    }
+    delay(100);
+    wdt_reset();
+}
